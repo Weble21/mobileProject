320cf320a9cae25a5c62df7bd742fd885d162524
Weble21 <kdk4270@naver.com>
2023-12-06 03:09:39 +0900

    구글맵 오류 발생 -> 네이버맵으로 api 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
-    implementation("com.google.android.gms:play-services-maps:18.1.0")
-    implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("com.naver.maps:map-sdk:3.15.0")
+
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
