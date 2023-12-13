286c18cfd2ad81274f79ee08087095b59dcb8dc2
Weble21 <kdk4270@naver.com>
2023-12-13 19:26:07 +0900

    지진해일 페이지 작업

--- app/src/main/java/com/example/project/Shelter_search.kt
+++ app/src/main/java/com/example/project/Shelter_search.kt
@@ -3,9 +3,11 @@ package com.example.project
 import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
+import android.view.View
+import android.webkit.WebView
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.LinearLayout
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.project.Models.shelter.Row_shelter
@@ -30,10 +32,55 @@ class Shelter_search : AppCompatActivity() {
             onBackPressed();
         }
 
-        getAPI()
+        val selectList: LinearLayout = findViewById(R.id.selectList)
+
+        var area: String = ""
+
+        val btnGangwon: Button = findViewById(R.id.btnGangwon)
+        btnGangwon.setOnClickListener {
+            Log.i("selectArea", "강원특별자치도")
+            area = "강원특별자치도"
+            selectList.visibility = View.GONE
+
+        }
+        val btnBusan: Button = findViewById(R.id.btnBusan)
+        btnBusan.setOnClickListener {
+            Log.i("selectArea", "강원특별자치도")
+            area = "부산광역시"
+            selectList.visibility = View.GONE
+        }
+        val btnGwangju: Button = findViewById(R.id.btnGwangju)
+        btnGwangju.setOnClickListener {
+            Log.i("selectArea", "광주광역시")
+            area = "광주광역시"
+            selectList.visibility = View.GONE
+        }
+        val btnGyeongsang: Button = findViewById(R.id.btnGyeongsang)
+        btnGyeongsang.setOnClickListener {
+            Log.i("selectArea", "경상북도")
+            area = "경상북도"
+            selectList.visibility = View.GONE
+        }
+        val btnJeonla: Button = findViewById(R.id.btnJeonla)
+        btnJeonla.setOnClickListener {
+            Log.i("selectArea", "전라남도")
+            area = "전라남도"
+            selectList.visibility = View.GONE
+        }
+        val btnUlsan: Button = findViewById(R.id.btnUlsan)
+        btnUlsan.setOnClickListener {
+            Log.i("selectArea", "울산광역시")
+            area = "울산광역시"
+            selectList.visibility = View.GONE
+        }
+
+
+        if(area != "") {
+            getAPI(area)
+        }
     }
     @Override
-    private fun getAPI() {
+    private fun getAPI(area: String) {
         if (Constants.isNetworkAvailable(this)) {
 
             val retrofit: Retrofit = Retrofit.Builder()
@@ -44,7 +91,7 @@ class Shelter_search : AppCompatActivity() {
             val service: ShelterService = retrofit
                 .create<ShelterService>(ShelterService::class.java)
 
-            fetchMessages(service)
+            fetchMessages(service, area)
         } else {
             Toast.makeText(
                 this@Shelter_search,
@@ -54,7 +101,7 @@ class Shelter_search : AppCompatActivity() {
         }
     }
 
-    private fun fetchMessages(service: ShelterService) {
+    private fun fetchMessages(service: ShelterService, area:String) {
         val listCall: Call<ShelterResponse> = service.getMessage(
             Constants.APP_ID, pageNo, numOfRows, "json"
         )
@@ -78,36 +125,50 @@ class Shelter_search : AppCompatActivity() {
                                     findViewById(R.id.messageList)
 
                                 for (i in 1 until rowShelterList.size) {
-
                                     val currentRow: Row_shelter = rowShelterList[i]
-                                    val currentRowCreateMsg: String =
-                                        "${currentRow.address}\n" +
-                                                "${currentRow.shelNm}\n" +
-                                                "수용가능 인원: ${currentRow.shelAv}\n" +
-                                                "대피소 분류: ${currentRow.shelDivType}"
+                                    if (currentRow.sidoName == area) {
 
+                                        val currentRowCreateMsg: String =
+                                            "${currentRow.address}\n" +
+                                                    "${currentRow.sidoName}\n" +
+                                                    "${currentRow.shelNm}\n" +
+                                                    "수용가능 인원: ${currentRow.shelAv}\n" +
+                                                    "대피소 분류: ${currentRow.shelDivType}"
 
+                                        //View 추가
+                                        val params = LinearLayout.LayoutParams(
+                                            LinearLayout.LayoutParams.MATCH_PARENT,
+                                            LinearLayout.LayoutParams.WRAP_CONTENT
+                                        )
+                                        params.setMargins(60, 0, 60, 50)
+                                        val textView = Button(this@Shelter_search).apply {
+                                            setBackgroundResource(R.drawable.rounded_background)
+                                            layoutParams = params
+                                            setPadding(30, 40, 30, 40)
+                                            gravity = Gravity.CENTER_VERTICAL
+                                        }
+                                        textView.text = currentRowCreateMsg
+                                        messageListLinearLayout.addView(textView)
+                                        messageListLinearLayout.visibility = View.VISIBLE
+                                        textView.setOnClickListener {
+                                            val webPageView = WebView(this@Shelter_search)
+                                            webPageView.loadUrl("https://map.naver.com/p/search/" + "${currentRow.address}")
 
-                                    //View 추가
-                                    val params = LinearLayout.LayoutParams(
-                                        LinearLayout.LayoutParams.MATCH_PARENT,
-                                        LinearLayout.LayoutParams.WRAP_CONTENT
-                                    )
-                                    params.setMargins(60, 0, 60, 50)
-                                    messageListLinearLayout.addView(TextView(this@Shelter_search).apply {
-                                        text = "$currentRowCreateMsg \n"
-                                        setBackgroundResource(R.drawable.rounded_background_2)
-                                        layoutParams = params
-                                        setPadding(30, 40, 30, 10)
-                                        gravity = Gravity.CENTER_VERTICAL
-                                    })
+                                            val layout = LinearLayout(this@Shelter_search)
+
+                                            layout.addView(webPageView)
+
+                                            setContentView(layout)
+                                        }
+
+                                    }
                                 }
                             } else {
                                 Log.e("err", "rowList err")
                             }
                         }
                         pageNo++
-                        fetchMessages(service)
+                        fetchMessages(service, area)
                     } else {
                         Log.e("err", "disasterMsgList err")
                     }
